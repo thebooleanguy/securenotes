0a0681a01f6d61542980a65566d79cadde5e8bdc
thebooleanguy <tharindus0601@gmail.com>
2025-12-16 11:57:23 +0530

    Generate Constructors, Getters and Setters for the Role Entity

--- src/main/java/com/thebooleanguy/securenotes/model/Note.java
+++ src/main/java/com/thebooleanguy/securenotes/model/Note.java
@@ -16,4 +16,39 @@ public class Note {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    // constructors, getters, setters
+
+    public Note() {
+    }
+
+    public Note(Long id, String content, User user) {
+        this.id = id;
+        this.content = content;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
