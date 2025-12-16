e6b6f99390ffbdea0d73f6c475b42241a4f66520
thebooleanguy <tharindus0601@gmail.com>
2025-12-16 10:35:06 +0530

    Initialize Spring Project

---
+++ src/test/java/com/thebooleanguy/securenotes/SecurenotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebooleanguy.securenotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurenotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
