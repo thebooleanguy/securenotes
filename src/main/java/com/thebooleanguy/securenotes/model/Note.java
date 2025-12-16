ddf539e5529c77c74571c1b44f12ebe1a00a95f8
thebooleanguy <tharindus0601@gmail.com>
2025-12-16 11:56:08 +0530

    Create Note Entity

---
+++ src/main/java/com/thebooleanguy/securenotes/model/Note.java
@@ -0,0 +1,19 @@
+package com.thebooleanguy.securenotes.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
