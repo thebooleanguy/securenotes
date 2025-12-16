bc6c1601e78d085427d5486ef61a5b5586d89871
thebooleanguy <tharindus0601@gmail.com>
2025-12-16 11:16:23 +0530

    Create User Entity

---
+++ src/main/java/com/thebooleanguy/securenotes/model/User.java
@@ -0,0 +1,22 @@
+package com.thebooleanguy.securenotes.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
