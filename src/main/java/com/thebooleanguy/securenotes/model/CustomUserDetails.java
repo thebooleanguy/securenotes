7812a0f961fc433beec7d93c9757523c15447fe4
thebooleanguy <tharindus0601@gmail.com>
2025-12-17 10:18:24 +0530

    Create CustomUserDetails Adapter with placeholder methods

---
+++ src/main/java/com/thebooleanguy/securenotes/model/CustomUserDetails.java
@@ -0,0 +1,32 @@
+package com.thebooleanguy.securenotes.model;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
