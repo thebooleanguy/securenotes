668ee9eb031c8689a2f199ec2a5adbb7098395a4
thebooleanguy <tharindus0601@gmail.com>
2025-12-17 10:31:11 +0530

    Implement CustomUserDetailsService adapter

---
+++ src/main/java/com/thebooleanguy/securenotes/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.thebooleanguy.securenotes.service;
+
+import com.thebooleanguy.securenotes.model.CustomUserDetails;
+import com.thebooleanguy.securenotes.model.User;
+import com.thebooleanguy.securenotes.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
