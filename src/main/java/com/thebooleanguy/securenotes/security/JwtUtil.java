3cd92c9ad46291d85dcaa7ca612eefc5fe95c211
thebooleanguy <tharindus0601@gmail.com>
2025-12-17 11:48:59 +0530

    Implement JWT token generation

---
+++ src/main/java/com/thebooleanguy/securenotes/security/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.thebooleanguy.securenotes.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "very_secret_key_change_later_32bytes_minimum!";
+    private final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
