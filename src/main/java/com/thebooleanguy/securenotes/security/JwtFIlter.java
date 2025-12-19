39cd137aba0ae3d205eae385a5b04142363e93a5
thebooleanguy <tharindus0601@gmail.com>
2025-12-19 07:54:48 +0530

    Create an authentication token and populate security context with it

--- src/main/java/com/thebooleanguy/securenotes/security/JwtFIlter.java
+++ src/main/java/com/thebooleanguy/securenotes/security/JwtFIlter.java
@@ -1,10 +1,14 @@
 package com.thebooleanguy.securenotes.security;
 
+import com.thebooleanguy.securenotes.model.User;
 import com.thebooleanguy.securenotes.service.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -32,10 +36,32 @@ public class JwtFIlter extends OncePerRequestFilter {
         String token = null;
         String username = null;
 
-        // Extract token from the header and then extract username from the token
+        // Extract token from the authorization header and then extract username from the token
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.substring(7);
             username = jwtUtil.extractUsername(token);
         }
+
+        if (username != null &&
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+            // Check JWT's validity
+            if (jwtUtil.isTokenValid(token, userDetails)) {
+
+                // Create an authentication token with our user's details
+                UsernamePasswordAuthenticationToken authToken  =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                // Populate security context with our authentication token
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
