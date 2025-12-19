bc439fc2fe8ee4befb7745fafe1ddc6881503d36
thebooleanguy <tharindus0601@gmail.com>
2025-12-19 19:09:47 +0530

    Fix missing @GetMapping in endpoint

--- src/main/java/com/thebooleanguy/securenotes/controller/NoteController.java
+++ src/main/java/com/thebooleanguy/securenotes/controller/NoteController.java
@@ -4,10 +4,7 @@ import com.thebooleanguy.securenotes.model.Note;
 import com.thebooleanguy.securenotes.service.NoteService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +23,7 @@ public class NoteController {
         return ResponseEntity.ok(noteService.createNote(note.getContent()));
     }
 
+    @GetMapping
     public List<Note> getMyNotes() {
         return noteService.getMyNotes();
     }
