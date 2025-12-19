8fc6955b0a9eb3717301e9af14020bebb165c97d
thebooleanguy <tharindus0601@gmail.com>
2025-12-19 08:41:56 +0530

    Create DTOs for Login

---
+++ src/main/java/com/thebooleanguy/securenotes/model/DTOs/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.thebooleanguy.securenotes.model.DTOs;
+
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/thebooleanguy/securenotes/model/DTOs/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.thebooleanguy.securenotes.model.DTOs;
+
+public class LoginResponse {
+    private String token;
+
+    public LoginResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
