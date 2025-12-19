4e63d69cf9543aa3f947d468912a1a83fb354f38
thebooleanguy <tharindus0601@gmail.com>
2025-12-19 19:21:31 +0530

    Implement a DTO for creating notes

---
+++ src/main/java/com/thebooleanguy/securenotes/model/DTOs/CreateNoteRequest.java
@@ -0,0 +1,9 @@
+package com.thebooleanguy.securenotes.model.DTOs;
+
+public class CreateNoteRequest {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+}
