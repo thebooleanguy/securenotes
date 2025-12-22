de3e91d2739848ac5035575d834ae83f20d1c35e
thebooleanguy <tharindus0601@gmail.com>
2025-12-22 13:33:24 +0530

    Secure endpoints by role

--- src/main/java/com/thebooleanguy/securenotes/controller/NoteController.java
+++ src/main/java/com/thebooleanguy/securenotes/controller/NoteController.java
@@ -4,6 +4,7 @@ import com.thebooleanguy.securenotes.model.DTOs.CreateNoteRequest;
 import com.thebooleanguy.securenotes.model.Note;
 import com.thebooleanguy.securenotes.service.NoteService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,8 +24,16 @@ public class NoteController {
         return ResponseEntity.ok(noteService.createNote(request.getContent()));
     }
 
+
     @GetMapping
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public List<Note> getMyNotes() {
         return noteService.getMyNotes();
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<Note> getAllNotesForAdmin() {
+        return noteService.getAllNotesForAdmin();
     }
 }
